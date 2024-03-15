907c64d5b31f4fe3a2191af4ba7b55b286360b2b
OnkarMalawade <132775903+OnkarMalawade@users.noreply.github.com>
2024-03-15 20:50:07 +0530

    Update p2p_A35.cc

--- p2p_A35.cc
+++ p2p_A35.cc
@@ -7,6 +7,9 @@
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
 
 // Adding namespace declaration
 using namespace ns3;
